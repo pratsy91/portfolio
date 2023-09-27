d1f0ea9cef3bdb2016881a9073899e75b0d4e101
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-27 18:19:26 +0530

    header

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Header />
+
+      <div className="content font-mont">{children}</div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
