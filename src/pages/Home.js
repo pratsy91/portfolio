c23a1858acf33ffcf4d73d55bdbba27d9e2e1b29
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-27 18:53:10 +0530

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,84 +1,216 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
-import { BsCodeSlash } from "react-icons/bs";
-
+import React, { useEffect } from "react";
+import Layout from "../components/Layout";
+import {
+  FaBootstrap,
+  FaCss3,
+  FaHtml5,
+  FaJsSquare,
+  FaNodeJs,
+  FaReact,
+} from "react-icons/fa";
+import AOS from "aos";
+AOS.init({
+  duration: 1000,
+});
 function Home() {
-  const [showMenu, setShowMenu] = useState("md:hidden");
-  const menuItems = [
-    {
-      title: "Home",
-      key: "/",
-    },
-    {
-      title: "Projects",
-      key: "/projects",
-    },
-    {
-      title: "Contact",
-      key: "/contact",
-    },
-  ];
-
-  const pathname = window.location.pathname;
-
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <div className="text-white font-mont fixed top-0 left-0 right-0 z-50">
-      <div
-        className={`flex bg-theme justify-between items-center p-2 shadow-lg ${
-          showMenu == "" && "md:flex-col"
-        } `}
-      >
-        <div className="flex justify-between items-center w-full">
-          <Link to="/">
-            <h1 className="text-4xl font-semibold hover:text-yellow-500 cursor-pointer flex gap-2">
-              <BsCodeSlash /> P S C
+    <Layout>
+      <div>
+        {/* Intro section */}
+        <div className="h-screen bg-theme">
+          <div
+            className="grid md:grid-cols-1 grid-cols-2 h-screen items-center border-4 md:border-0
+            mx-10 z-10 bg-theme
+          border-white transform rotate-12 md:rotate-0"
+          >
+            <div className="h-1/2">
+              <lottie-player
+                src="https://assets4.lottiefiles.com/packages/lf20_kkflmtur.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+
+            <div className="font-bold text-white md:px-5">
+              <h1 className="text-7xl md:text-4xl" data-aos="slide-right">
+                Hi , I am <b className="text-yellow-500">PRATAP</b>
+              </h1>
+              <h1 className="text-4xl md:text-xl" data-aos="slide-left">
+                FullStack <b className="text-red-500">Developer</b>
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        {/* Technologies */}
+
+        <div className="mt-20">
+          <h1
+            className="text-4xl text-blue-800 font-bold text-center my-8"
+            data-aos="slide-up"
+          >
+            Technologies I USE
+          </h1>
+          <div className="grid md:grid-cols-1 grid-cols-3">
+            <FaReact
+              size={180}
+              color="cyan"
+              className="w-full text-center mt-20"
+            />
+            <FaNodeJs
+              size={180}
+              color="green"
+              className="w-full text-center mt-20 animate-bounce"
+            />
+            <FaBootstrap
+              size={180}
+              color="blue"
+              className="w-full text-center mt-20 "
+            />
+            <FaJsSquare
+              size={180}
+              color="green"
+              className="w-full text-center mt-20 animate-bounce"
+            />
+            <FaHtml5
+              size={180}
+              color="orange"
+              className="w-full text-center mt-20"
+            />
+            <FaCss3
+              size={180}
+              color="blue"
+              className="w-full text-center mt-20 animate-bounce"
+            />
+          </div>
+        </div>
+
+        {/* {Javscript Buff} */}
+        <div className="my-20">
+          <div className="text-center h-52 bg-primary">
+            <h1 className="text-white font-bold text-4xl py-10">
+              Yes You Are Right...I am Javascript Buff
             </h1>
-          </Link>
+          </div>
 
-          <FaBars
-            onClick={() => {
-              if (showMenu == "md:hidden") {
-                setShowMenu("");
-              } else {
-                setShowMenu("md:hidden");
-              }
-            }}
-            className="lg:hidden xl:hidden 2xl:hidden md:flex cursor-pointer"
-          />
+          <div className="md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white">
+            <div className="h-96" data-aos="zoom-in">
+              <lottie-player
+                src="https://assets9.lottiefiles.com/packages/lf20_sSF6EG.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+
+            <p className="text-xl my-5 font-semibold md:px-5 px-14 py-10">
+              JavaScript is one of the most top-ranked programming languages
+              because of its ubiquitous use on all platforms and mass adoption.
+              Main Use Cases: Web Development.
+            </p>
+          </div>
         </div>
 
-        <div className="flex md:hidden">
-          {menuItems.map((item) => {
-            return (
-              <li
-                className={`list-none mx-5 px-5 ${
-                  item.key == pathname && "bg-white text-black rounded-md"
-                }`}
-              >
-                <Link to={`${item.key}`}>{item.title}</Link>
-              </li>
-            );
-          })}
+        {/* {Dev stack section} */}
+        <div className="my-20">
+          <div className="text-center h-52 bg-red-500">
+            <h1 className="text-white font-bold text-4xl py-10">
+              My DEV Stack
+            </h1>
+          </div>
+
+          <div className="md:mx-5 mx-32 shadow-2xl bg-gray-50 -mt-20 rounded-lg hover:bg-gray-700 hover:text-white">
+            <div className="h-96" data-aos="zoom-in">
+              <lottie-player
+                src="https://assets2.lottiefiles.com/packages/lf20_aptscmnx.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+
+            <div className="grid md:grid-cols-1 grid-cols-3 p-5">
+              <div>
+                <h1 className="text-xl font-bold">Front End</h1>
+                <hr />
+                <p className="font-semibold mt-2">HTML/CSS</p>
+                <p className="font-semibold mt-2">React</p>
+
+                <p className="font-semibold mt-2">Javascript</p>
+                <p className="font-semibold mt-2">Redux</p>
+              </div>
+
+              <div className="text-center">
+                <h1 className="text-xl font-bold">UI / UX</h1>
+                <hr />
+                <p className="font-semibold mt-2">Bootstrap</p>
+                <p className="font-semibold mt-2">Tailwind</p>
+
+                <p className="font-semibold mt-2">Ant Design</p>
+                <p className="font-semibold mt-2">Material UI</p>
+                <p className="font-semibold mt-2">Semantic UI</p>
+              </div>
+
+              <div className="text-right">
+                <h1 className="text-xl font-bold">Backend and Databases</h1>
+                <hr />
+                <p className="font-semibold mt-2">Node JS</p>
+                <p className="font-semibold mt-2">Express JS</p>
+
+                <p className="font-semibold mt-2">PHP</p>
+                <p className="font-semibold mt-2">My SQL</p>
+                <p className="font-semibold mt-2">Mongo DB</p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div
-          className={`mt-5 md:flex items-center justify-start w-full flex-col lg:hidden 2xl:hidden xl:hidden ${showMenu}`}
-        >
-          {menuItems.map((item) => {
-            return (
-              <li
-                className={`list-none mt-5 ${
-                  item.key == pathname && "bg-white text-black rounded-md px-5"
-                }`}
-              >
-                <Link to={`${item.key}`}>{item.title}</Link>
-              </li>
-            );
-          })}
+        {/* Dev Info */}
+
+        <div>
+          <h1 className="text-4xl text-gray-500 text-center font-bold">
+            Who is Pratap ?
+          </h1>
+
+          <div className="h-screen relative text-gray-800">
+            <div className="h-full">
+              <lottie-player
+                src="https://assets3.lottiefiles.com/packages/lf20_dcatp5cr.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <h1 className="text-2xl font-bold">
+                Hi , Hello , Namaste...
+                <hr />
+                <pre className="text-xl md:text-sm my-5 font-mont font-semibold">
+                  {JSON.stringify(
+                    {
+                      name: "Pratap Singh Chouhan",
+                      age: null,
+                      gender: "Male",
+                      country: "INDIA",
+                    },
+                    null,
+                    2
+                  )}
+                </pre>
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
